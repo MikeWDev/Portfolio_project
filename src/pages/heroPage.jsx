1efa5ff545d7bb48d99d767f38facbd24a0e91e7
Michal <mkocik.dev@gmail.com>
2023-10-16 16:04:19 +0100

    Finished navbar component

--- src/pages/heroPage.jsx
+++ src/pages/heroPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "../components/component-style.css";
+import "../pages/pageStyles.css";
 function HeroPage() {
   <section className="section-hero">
-    <div className="hero-page container grid"></div>
+    <div className="hero-page container grid">
+      <button>FASf</button>
+    </div>
   </section>;
 }
 
