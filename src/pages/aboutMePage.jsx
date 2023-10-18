fefda2f873234090859069da529050be843d040d
Michal <mkocik.dev@gmail.com>
2023-10-18 10:25:33 +0100

    Created skeleton for aboutMePage

--- src/pages/aboutMePage.jsx
+++ src/pages/aboutMePage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function AboutMePage() {
-  return <></>;
+  return (
+    <>
+      <section className="about-me-section">
+        <div className="about-page container gird">
+          <div className="grid about-img">
+            <img src="../assets/img/gosia-logo.jpg" alt="" />
+          </div>
+          <div className="grid about-content">
+            <div className="about-text">
+              <div className="about-disc">
+                {" "}
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab,
+                ipsa at quasi sed vitae dolore? Autem accusamus{" "}
+              </div>
+              <div className="about-adress">Adress</div>
+            </div>
+            <div className="about-skills">My skills</div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default AboutMePage;
