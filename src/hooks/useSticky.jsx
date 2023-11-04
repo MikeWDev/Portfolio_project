6ddfad9c69eead86329bc682f736b2a7caa58502
Michal <mkocik.dev@gmail.com>
2023-11-04 19:55:41 +0100

    Control commit

--- src/hooks/useSticky.jsx
+++ src/hooks/useSticky.jsx
@@ -3,27 +3,27 @@ const useSticky = () => {
   const [stickyClass, setStickyClass] = useState("");
   const [paddingClass, setPaddingClass] = useState("");
 
-  const obs = new IntersectionObserver(
-    function (entries) {
-      const ent = entries[0];
+  // const obs = new IntersectionObserver(
+  //   function (entries) {
+  //     const ent = entries[0];
 
-      if (!ent.isIntersecting) {
-        setStickyClass("sticky");
-        setPaddingClass("sticky-padding");
-      } else if (ent.isIntersecting) {
-        setStickyClass("");
-        setPaddingClass("");
-      }
-    },
-    {
-      root: null,
-      threshold: 0,
-    }
-  );
-  const heroElement = document.querySelectorAll(".section-hero");
-  heroElement.forEach((el) => {
-    obs.observe(el);
-  });
+  //     if (!ent.isIntersecting) {
+  //       setStickyClass("sticky");
+  //       setPaddingClass("sticky-padding");
+  //     } else if (ent.isIntersecting) {
+  //       setStickyClass("");
+  //       setPaddingClass("");
+  //     }
+  //   },
+  //   {
+  //     root: null,
+  //     threshold: 0,
+  //   }
+  // );
+  // const heroElement = document.querySelectorAll(".section-hero");
+  // heroElement.forEach((el) => {
+  //   obs.observe(el);
+  // });
 
   return { stickyClass, paddingClass };
 };
